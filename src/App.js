049297377164ce36f13f4bfa461c3308e966f8c3
Andrej <andrej@loka.com>
2022-04-27 18:07:02 +0200

    proba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         BYE <code>src/App.js</code> and save to reload.
+         BYEa <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
